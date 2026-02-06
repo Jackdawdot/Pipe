cc9aef8c58f7af91935cc0b093d6e0311618c351
Jackdawdot <strong.zhigunov@mail.ru>
2026-02-06 13:43:01 +0700

    Minor upds

--- scripts/container_build.sh
+++ scripts/container_build.sh
@@ -1,33 +1,11 @@
 #!/bin/bash
 set -e
 
-BUILD_TYPE="$1"
+TYPE="$1"
 REVISION="$2"
 DEB_VERSION="$3"
-NGINX_TAG="$4
 
-if [ -z "$BUILD_TYPE" ] || [ -z "$REVISION" ] || [ -z "$DEB_VERSION" ] || [ -z "$NGINX_TAG" ]; then
-  echo "Usage: container_build.sh "<release|debug|coverage> <revision> <deb_version> <nginx_tag>"
-  exit 1
-fi
-
-SRC_DIR="/workspace/src/nginx"
-ARTIFACTS_DIR="/workspace/artifacts"
-STATE_DIR="/workspace/state"
-
-mkdir -p /workspace/src
-mkdir -p "$ARTIFACTS_DIR/deb" "$ARTIFACTS_DIR/debug" "$ARTIFACTS_DIR/coverage"
-
-if [ ! -d "$SRC_DIR/.git" ]; then
-  echo "[INFO] Cloning nginx..."
-  git clone https://github.com/nginx/nginx.git "$SRC_DIR"
-fi
-
-cd "$SRC_DIR"
-git fetch --tags
-git checkout "$NGINX_TAG"
-
-case "$BUILD_TYPE" in
+case "$TYPE" in
   release)
     /workspace/scripts/build_release.sh "$REVISION" "$DEB_VERSION"
     ;;
@@ -38,7 +16,7 @@ case "$BUILD_TYPE" in
     /workspace/scripts/build_coverage.sh "$REVISION" "$DEB_VERSION"
     ;;
   *)
-    echo "Unknown build type: $BUILD_TYPE"
+    echo "Unknown build type: $TYPE"
     exit 1
     ;;
 esac
